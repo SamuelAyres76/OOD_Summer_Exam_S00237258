c064d73d5219c51e80e27ac2d8f15ce54e7b3ad3
GreenThunder <123633616+SamuelAyres76@users.noreply.github.com>
2024-05-13 13:08:33 +0100

    Q1 (b) + (c) Two classes created

---
+++ Restaurant App/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+
+    }
+}
